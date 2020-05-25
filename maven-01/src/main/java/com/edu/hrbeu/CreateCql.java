ec3c2af732734e7b22767320dc62d0e651b62890
sunshinenough <1448143444@qq.com>
2020-05-25 13:32:07 +0800

    test change

--- maven-01/src/main/java/com/edu/hrbeu/CreateCql.java
+++ maven-01/src/main/java/com/edu/hrbeu/CreateCql.java
@@ -2,7 +2,7 @@ package com.edu.hrbeu;
 
 public class CreateCql {
 	public String createTypeOne(String itemname,String rulescode){
-//		匹配语句
+//		匹配
 		String cql = "MATCH (m:规则)-[r:relation]->(n:object) where n.Name ='" + itemname + "'and m.RulesCode = '" + rulescode + "' return r.relation";
 		return cql;
 		
