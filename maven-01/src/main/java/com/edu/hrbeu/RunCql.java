ad068fa0da6235c52e69de42d001b95b604ef341
chenbin <chenbin@DESKTOP-6HRE4C3>
2020-05-25 21:08:04 +0800

    test

--- maven-01/src/main/java/com/edu/hrbeu/RunCql.java
+++ maven-01/src/main/java/com/edu/hrbeu/RunCql.java
@@ -20,6 +20,7 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 public class RunCql {
 	public static void main(String[] args){
+		//提交测试
 		String predir = "E:\\json_to_rdf\\处方.xlsx";
 		String resdir = "E:\\json_to_rdf\\登记表.xlsx";
 		
