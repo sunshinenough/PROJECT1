2db892d9c76f417669a1d7adea491e7c941ca9a7
sunshinenough <1448143444@qq.com>
2020-05-25 21:38:42 +0800

    test_change3

--- maven-01/src/main/java/com/edu/hrbeu/RunCql.java
+++ maven-01/src/main/java/com/edu/hrbeu/RunCql.java
@@ -20,11 +20,11 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 public class RunCql {
 	public static void main(String[] args){
-		//提交测试1
-//		String predir = "E:\\json_to_rdf\\处方.xlsx";
-//		String resdir = "E:\\json_to_rdf\\登记表.xlsx";
-		String predir = "D:\\文件\\医保业务规则\\医保业务规则排序后\\门诊处方表-副本.xlsx";
-		String regdir = "D:\\文件\\医保业务规则\\医保业务规则排序后\\门诊登记表-副本.xlsx";
+		
+		String predir = "E:\\json_to_rdf\\处方.xlsx";
+		String regdir = "E:\\json_to_rdf\\登记表.xlsx";
+//		String predir = "D:\\文件\\医保业务规则\\医保业务规则排序后\\门诊处方表-副本.xlsx";
+//		String regdir = "D:\\文件\\医保业务规则\\医保业务规则排序后\\门诊登记表-副本.xlsx";
 		
 		new RunCql().check(predir,regdir);
 	}
