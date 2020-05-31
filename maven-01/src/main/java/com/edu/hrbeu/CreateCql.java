10a8af42932c4bbccea3dbca0614a81513df4b09
SWEETY-GIFT <522188762@qq.com>
2020-05-31 14:20:43 +0800

    11

--- maven-01/src/main/java/com/edu/hrbeu/CreateCql.java
+++ maven-01/src/main/java/com/edu/hrbeu/CreateCql.java
@@ -1,17 +1,37 @@
 package com.edu.hrbeu;
 
+
 public class CreateCql {
 	public String createTypeOne(String itemname,String rulescode){
 //		语句生成
+		
 		String cql = "MATCH (m:规则)-[r:relation]->(n:object) where n.Name ='" + itemname + "'and m.RulesCode = '" + rulescode + "' return r.relation";
 		return cql;
 		
 	}
 	public String createTypeTwo(String ItemCode,String rulescode){
 //		语句生成
-		String cql = "MATCH (m:规则)-[r:relation]->(n:objectCode) where n.Code ='" + ItemCode + "'and m.RulesCode = '" + rulescode + "' return r.LimitPrice";      
+		String cql = "MATCH (m:规则)-[r:relation]->(n:objectCode) where n.Code ='" + ItemCode + "'and m.RulesCode = '" + rulescode + "' return r.PRICE";      
+		return cql;	
+		
+	}
+	public String createTypeThree(String itemCode,String itemCode2){
+		
+		String cql = "MATCH (m:objectCode)-[r:relation]->(n:objectCode) where n.Code ='" + itemCode + "'and m.Code = '" + itemCode2 + "' return r.relation";
+		
+		//药品名来判断，备选
+//		String cql = "MATCH (m:object)-[r:relation]->(n:object) where n.Name ='" + itemCode + "'and m.Name = '" + itemCode2 + "' return r.relation";
+		
+		return cql;	
+		
+	}
+	public String createMain(){
+//		语句生成
+		
+		String cql = "MATCH (n:cql) RETURN n.type,n.label1,n.label2,n.label1_code,n.label2_code,n.judgdata,n.judgtype,n.result";
 		return cql;
 		
 	}
 
 }
+
--- maven-01/src/main/java/com/edu/hrbeu/RunCql.java
+++ maven-01/src/main/java/com/edu/hrbeu/RunCql.java
@@ -1,5 +1,7 @@
 package com.edu.hrbeu;
+import java.awt.List;
 import java.io.File;
+import java.util.ArrayList;
 
 import org.apache.lucene.analysis.miscellaneous.WordDelimiterFilter;
 import org.apache.poi.ss.usermodel.Cell;
@@ -7,6 +9,8 @@ import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.neo4j.cypher.internal.frontend.v2_3.perty.recipe.Pretty.nestWith;
 import org.neo4j.driver.v1.AuthTokens;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
@@ -18,33 +22,46 @@ import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.omg.CORBA.PRIVATE_MEMBER;
+import java.util.*;
+import io.netty.channel.group.ChannelMatcher;
+import scala.collection.mutable.LinkedList;
 
 
 public class RunCql {
+	
+	private static CreateCql createCql = new CreateCql();
+	private static RunCql runCql = new RunCql();
 	public static void main(String[] args){
 		//上传测试
-		//测试2
 		
-		String predir = "E:\\json_to_rdf\\处方.xlsx";
-		String regdir = "E:\\json_to_rdf\\登记表.xlsx";
-		//测试1
-		//测试3
+		String predir = "D:\\业务数据\\处方.xlsx";
+		String regdir = "D:\\业务数据\\登记表.xlsx";
 
-	
-
-		new RunCql().check(predir,regdir);
+		runCql.check(predir,regdir);
 	}
+	
 	//private final Session session = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "123456")).session();
 	public StatementResult runCql(String itemname,String rulescode,Session session){
-		return session.run(new CreateCql().createTypeOne(itemname, rulescode));
+//		CreateCql createCql = new CreateCql();
+		return session.run(createCql.createTypeOne(itemname, rulescode));
 	}
 	
 	public StatementResult runCqlTwo(String itemcode,String rulescode,Session session) {
-		return session.run(new CreateCql().createTypeTwo(itemcode, rulescode));
+		return session.run(createCql.createTypeTwo(itemcode, rulescode));
+	}
+	
+	public StatementResult runCqlThree(String itemCode,String itemCode2,Session session) {
+		return session.run(createCql.createTypeThree(itemCode, itemCode2));
+	}
+	
+	public StatementResult runCqlmain(Session session){
+		return session.run(createCql.createMain());
 	}
 	 
 	public void check(String preDir,String regDir){
 		//reglist是登记表，prelist是处方表
+//		RunCql runCql = new RunCql();
 		String[][] prelist = null;
 		String[][] reglist = null;
 		try {
@@ -60,17 +77,55 @@ public class RunCql {
 		int j = 1;
 		Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "123456"));
 		Session session = driver.session();
+		//二维数组cqlRules存储下面while循环中的cqlmain,目前是四种规则
+		//String[][] cqlRules = new String[4][8];
+		ArrayList<String[]> cqlmains = new ArrayList<>();
 		//注意判断字符串是否相等用equals方法
+		int index = 0;
+		StatementResult resultmain = runCql.runCqlmain(session);
+		while(resultmain.hasNext()){
+			Record recordMain = resultmain.next();
+			String cqlmaintemp = recordMain.values().toString().replace("\"", "").replace("[", "").replace("]", "").replace(" ", "");
+			String[] cqlmain = cqlmaintemp.split(",");
+			/*
+			for(int a=0;a<cqlmain.length;a++){
+				System.out.println(cqlmain[a]);
+				//cqlRules[index][a] = cqlmain[a];
+			}
+			*/
+			index ++;
+			cqlmains.add(cqlmain);
+		}
+		/*
+		for(int x = 0;x < cqlRules.length;x ++){
+			for(int y = 0;y < cqlRules[0].length;y ++){
+				System.out.println(cqlRules[x][y]);
+			}
+		}
+		*/
+		for(int x = 0;x < cqlmains.size();x ++){
+			for(int y = 0;y < 8;y ++){
+				System.out.println(cqlmains.get(x)[y]);
+			}
+		}
 		while(i < reglist.length){
+			//定义患者用药、项目列表
+			String[] itemlist = new String[100];
+			int itemindex = 0;
 			while(j < prelist.length && reglist[i][3].equals(prelist[j][0])){
 				int age = Integer.parseInt(reglist[i][8]);
 				//System.out.println(age + "000000000");
 				String itemname = prelist[j][4];
 				String precode = prelist[j][1];
 				String itemnode = prelist[j][3];
+				
+				//记录患者用药
+				itemlist[itemindex] = itemnode;
+				itemindex ++;
+				
 				double price =Double.parseDouble(prelist[j][13]); 
 //				辅药审核
-				StatementResult resultC007 = new RunCql().runCql(itemname, "C007",session);
+				StatementResult resultC007 = runCql.runCql(itemname, "C007",session);
 				if(resultC007.hasNext()){
 					System.out.println("处方：" + precode + "药品：" + itemname + "辅药使用情况审核异常！");
 				}
@@ -78,14 +133,14 @@ public class RunCql {
 //				老年人禁忌
 				if(age > 70 && age < 150){
 					//String itemnamec003 = prelist[j][4];
-					StatementResult resultC003 = new RunCql().runCql(itemname, "C003",session);
+					StatementResult resultC003 = runCql.runCql(itemname, "C003",session);
 					if(resultC003.hasNext()){
 						System.out.println("处方：" + precode + "药品：" + itemname + "老年人用药禁忌审核异常！");
 					}
 				}
 				
 				//药品限价审核
-				StatementResult resultB004 = new RunCql().runCqlTwo(itemnode, "B004", session);
+				StatementResult resultB004 = runCql.runCqlTwo(itemnode, "B004", session);
 				if(resultB004.hasNext()){
 					//获取药品限定价格
 					Record recordB004 = resultB004.next();
@@ -94,11 +149,28 @@ public class RunCql {
 					double price_check = Double.parseDouble(priceB004);
 //					System.out.println(price_check);
 					if(price > price_check)
-						System.out.println("处方： "+ precode + " 药品编号：" + itemnode + " 限定价格： " + price_check + " 药品限价审核未通过");
+						System.out.println("门诊登记号： "+ precode + " 药品编号：" + itemnode + " 限定价格： " + price_check + " 药品限价审核未通过");
 				}
 				
 				//System.out.println("处方：" + precode + "药品：" + itemname + "正常");
 				j ++;
+			}
+			//判断两种药是否不能同时出现在一个药方中
+			int judgei,judgej;
+			for(judgei = 0;judgei < (itemindex-1);judgei ++){
+				for(judgej = (judgei+1);judgej < itemindex;judgej++){
+					//执行查询
+					StatementResult resultUnion = runCql.runCqlThree(itemlist[judgei], itemlist[judgej], session);
+					if(resultUnion.hasNext()){
+						Record recordUnion = resultUnion.next();
+						String union = recordUnion.values().toString().replace("\"", "").replace("[", "").replace("]", "");
+						System.out.println("登记编号： "+ reglist[i][3] + " 对应的处方中同时使用了药品1：" + itemlist[judgei] + " 药品2： " + itemlist[judgej] + union );
+					}
+//					System.out.println(itemlist[judgei]);
+//					if(itemlist[judgei].equals("X-J01FA-K067-E001-1V")){
+//						System.out.println("登记编号： "+ reglist[i][3] + " 对应的处方中同时使用了药品1：" + itemlist[judgei] + " 药品2： " + itemlist[judgej] + " 药品不能同时使用");
+//					}
+				}
 			}
 			i ++;
 		}
