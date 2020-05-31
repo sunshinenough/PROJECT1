1a8f3645ace9eb8accb7697c64d075a4f1edeafe
sunshinenough <1448143444@qq.com>
2020-05-31 14:16:38 +0800

    1111

--- maven-01/src/main/java/com/edu/hrbeu/CreateCql.java
+++ maven-01/src/main/java/com/edu/hrbeu/CreateCql.java
@@ -1,26 +1,23 @@
 package com.edu.hrbeu;
 
+
 public class CreateCql {
 	public String createTypeOne(String itemname,String rulescode){
 //		语句生成
 		
-		String cql = "MATCH (m:规则)-[r:relation]->(n:object) where n.Name ='" + itemname + "'and m.RulesCode = '" + rulescode + "' return properties(r)";
+		String cql = "MATCH (m:规则)-[r:relation]->(n:object) where n.Name ='" + itemname + "'and m.RulesCode = '" + rulescode + "' return r.relation";
 		return cql;
 		
 	}
 	public String createTypeTwo(String ItemCode,String rulescode){
 //		语句生成
-//<<<<<<< HEAD
 		String cql = "MATCH (m:规则)-[r:relation]->(n:objectCode) where n.Code ='" + ItemCode + "'and m.RulesCode = '" + rulescode + "' return r.PRICE";      
-//=======
-//		String cql = "MATCH (m:规则)-[r:relation]->(n:objectCode) where n.Code ='" + ItemCode + "'and m.RulesCode = '" + rulescode + "' return properties(r)";      
-//>>>>>>> branch 'master' of https://github.com/sunshinenough/PROJECT1.git
 		return cql;	
 		
 	}
 	public String createTypeThree(String itemCode,String itemCode2){
 		
-		String cql = "MATCH (m:objectCode)-[r:relation]->(n:objectCode) where n.Code ='" + itemCode + "'and m.Code = '" + itemCode2 + "' return properties(r)";
+		String cql = "MATCH (m:objectCode)-[r:relation]->(n:objectCode) where n.Code ='" + itemCode + "'and m.Code = '" + itemCode2 + "' return r.relation";
 		
 		//药品名来判断，备选
 //		String cql = "MATCH (m:object)-[r:relation]->(n:object) where n.Name ='" + itemCode + "'and m.Name = '" + itemCode2 + "' return r.relation";
@@ -37,3 +34,4 @@ public class CreateCql {
 	}
 
 }
+
